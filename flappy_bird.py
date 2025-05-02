4cc9134f87e6120d4ba3700ba9b56e6f20437e2e
Pavol Ulicny <pavolko.u@gmail.com>
2025-05-02 20:06:44 +0200

    Update flappy_bird.py

--- flappy_bird.py
+++ flappy_bird.py
@@ -5,7 +5,7 @@ import random
 import os
 
 #variables 
-global folder, running, start_screen
+global folder, running, start_screen, high_score_set
 window_width=550
 window_height=650
 running=False
@@ -33,7 +33,12 @@ pipe1_counted=False
 pipe2_counted=False
 pipe3_counted=False
 high_score=0
+high_score_set=False
+high_score_name=[]
+high_score_num=[]
+low_high_score=0
 score=0
+name_input=""
 
 #func that checks if a txt file is empty
 def is_empty(file):
@@ -41,18 +46,27 @@ def is_empty(file):
         return False
     else:
         return True
-    
+
 #paths
 bird_path=os.path.join(folder,"bird.png")
 pipe_path=os.path.join(folder,"pipe.png")
 
-#loads high score from file
+#loads high score from file ....................................................................................................................
 high_score_file=open(f"{folder}/high_score.txt", "a")
 high_score_file.close()
 if not is_empty(f"{folder}/high_score.txt"):
     high_score_file=open(f"{folder}/high_score.txt", "r")
-    high_score=int(high_score_file.read())
+    high_score_table=high_score_file.read()
+    high_score_list=high_score_table.splitlines()
+    high_score_name=[]
+    high_score_num=[]
+    for i in high_score_list:
+        var=i.split("/")
+        high_score_name.append(var[0])
+        high_score_num.append(var[1])
+    high_score=high_score_num[0]
     high_score_file.close()
+    high_score_num=[int(s) for s in high_score_num]
 
 #initializing pygame
 pygame.init()
@@ -78,23 +92,28 @@ game_over_text=font1.render("Game over",True,text_color)
 restart_text=font2.render("Press backspace to retry",True,text_color)
 high_score_text=font2.render(f"High score: {high_score}",True,text_color)
 score_text=font2.render(f"Score: {score}",True,text_color)
- 
-#create window
+high_scores_text=font2.render(f"Top 5 highest scores:",True,text_color)
+you_got_text=font2.render(f"You got one of the top 5 highest scores!",True,text_color)
+insert_name_text=font2.render(f"Insert a 3 letter name:",True,text_color)
+
+
+#create window 
 window1=pygame.display.set_mode((window_width,window_height))
 pygame.display.set_caption("Flappy bird")
 clock=pygame.time.Clock()
 
-#game over function
+#game over function ....................................................................................................................
 def game_over():
-    global running,score,high_score
-    if score>high_score:
-        high_score=score
-    high_score_file=open(f"{folder}/high_score.txt","w")
-    high_score_file.write(str(high_score))
-    high_score_file.close()
+    global running,score,high_score,high_score_set,low_high_score,high_score_name,high_score_num
+    if len(high_score_name)>0:
+        low_high_score=high_score_num[-1]
+        if score>low_high_score or len(high_score_name)<5 and score>0:
+            high_score_set=True
+    elif score>0:
+        high_score_set=True
     running=False
     
-#restart game function
+#restart game function ....................................................................................................................
 def restart_func():
     
     #reset variables
@@ -116,7 +135,7 @@ def restart_func():
     #run main loop
     running=True
 
-#start screen loop
+#start screen loop ....................................................................................................................
 while start_screen:
 
     #set fps
@@ -153,7 +172,7 @@ while start_screen:
     #update the window
     pygame.display.update()
     
-#game loop
+#game loop ....................................................................................................................
 while True:
     
     #main game loop
@@ -249,8 +268,99 @@ while True:
         #displays all updates that happened
         pygame.display.update()
         
-    #game over/restart screen
-    while not running:
+    #high score window (set name screen) ....................................................................................................................
+    while not running and high_score_set:
+        
+        #set fps
+        clock.tick(20)
+        
+        #sets window color
+        window1.fill((135,206,235))
+        
+        #text
+        name_display=font2.render(name_input + "|",True,text_color)
+        window1.blit(name_display,(255-(len(name_input)*6.3),240))
+        window1.blit(you_got_text,(60,100))
+        score_text=font2.render(f"Score: {score}",True,text_color)
+        window1.blit(score_text,(220,150))
+        window1.blit(insert_name_text,(145,200))
+        
+        #checking for events
+        for event in pygame.event.get():
+            
+            #exit the app
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+            #binds
+            if event.type==pygame.KEYDOWN:
+                
+                if event.key==pygame.K_ESCAPE:
+                    high_score_set=False
+                    name_input=""
+                    num=0
+                    
+                if event.key==pygame.K_RETURN and len(name_input)>0 and name_input not in high_score_name:
+                    
+                    #process scores
+                    num=0
+                    for i in high_score_num:
+                        if score>i:
+                            num+=1
+                            
+                    if num>0:
+                        high_score_name.insert(-num,name_input)
+                        high_score_num.insert(-num,score)
+                    else:
+                        high_score_name.append(name_input)
+                        high_score_num.append(score)
+
+                    #make the high score lists 5 items long
+                    high_score_name = high_score_name[:5]
+                    high_score_num = high_score_num[:5]
+                    
+                    #write the scores into a txt file
+                    high_score_file=open(f"{folder}/high_score.txt","w")
+                    for idx,i in enumerate(high_score_name):
+                        high_score_file.write(f"{i}/{high_score_num[idx]}\n")
+                    high_score_file.close()
+                    
+                    #reset some variables
+                    high_score_set=False
+                    name_input=""
+                    num=0
+                    
+                    #load the high scores 
+                    high_score_file=open(f"{folder}/high_score.txt", "a")
+                    high_score_file.close()
+                    if not is_empty(f"{folder}/high_score.txt"):
+                        high_score_file=open(f"{folder}/high_score.txt", "r")
+                        high_score_table=high_score_file.read()
+                        high_score_list=high_score_table.splitlines()
+                        high_score_name=[]
+                        high_score_num=[]
+                        for i in high_score_list:
+                            var=i.split("/")
+                            high_score_name.append(var[0])
+                            high_score_num.append(var[1])
+                        high_score=high_score_num[0]
+                        high_score_file.close()
+                        high_score_num=[int(s) for s in high_score_num]
+
+                elif event.key==pygame.K_BACKSPACE:
+                    name_input=name_input[:-1]
+                    
+                else:
+                    if len(name_input)<3 and event.unicode.isalpha():
+                        name_input+=event.unicode
+                        name_input=name_input.upper()
+                    
+        #update the window
+        pygame.display.update()
+        
+    #game over/restart screen ....................................................................................................................
+    while not running and not high_score_set:
         
         #set fps
         clock.tick(20)
@@ -259,19 +369,28 @@ while True:
         window1.fill((135,206,235))
         
         #render text
-        window1.blit(game_over_text,(100,110))
-        window1.blit(restart_text,(130,200))
+        window1.blit(game_over_text,(100,5))
+        window1.blit(restart_text,(130,70))
         score_text=font2.render(f"Score: {score}",True,text_color)
-        window1.blit(score_text,(220,270))
-        high_score_text=font2.render(f"High score: {high_score}",True,text_color)
-        window1.blit(high_score_text,(190,330))
+        window1.blit(score_text,(220,115))
+        if len(high_score_name)>0:
+            high_scores_text=font2.render(f"Top 5 highest scores:",True,text_color)
+        else:
+            high_scores_text=font2.render("",True,text_color)
+        window1.blit(high_scores_text,(140,160))
+        high_score_y=240
+        for idx,i in enumerate(high_score_name):
+            high_score_text=font2.render(i,True,text_color)
+            high_score_text2=font2.render(str(high_score_num[idx]),True,text_color)
+            window1.blit(high_score_text,(100,high_score_y))
+            window1.blit(high_score_text2,(390-(10*len(str(high_score_num[idx]))),high_score_y))
+            high_score_y+=70
         
         #checking for events
         for event in pygame.event.get():
             
             #exit the app
             if event.type==pygame.QUIT:
-                start_screen=False
                 pygame.quit()
                 sys.exit()
                 
